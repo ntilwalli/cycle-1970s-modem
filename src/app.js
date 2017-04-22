c42d988d4c74a10d49b14d76c47e844604c04104
Thomas Belin <tom@doyoubuzz.com>
2017-04-22 15:23:06 +0200

    Fake driver

--- src/app.js
+++ src/app.js
@@ -4,38 +4,62 @@ import xs from 'xstream'
 const audioCtx = new AudioContext();
 const analyser = audioCtx.createAnalyser();
 
-const BUFF_SIZE = 16384;
+const usersImages = [
+    "felix",
+    "salut",
+    "bonjour"
+]
 
 function startMicrophone(stream){
-    source = audioCtx.createMediaStreamSource(stream);
+    const source = audioCtx.createMediaStreamSource(stream);
     source.connect(analyser);
 
-    analyser.fftSize = 2048;
+    analyser.fftSize = 256;
     var bufferLength = analyser.frequencyBinCount;
     var dataArray = new Uint8Array(bufferLength);
 
-    function draw() {
-        analyser.getByteTimeDomainData(dataArray);
 
-        console.log(dataArray);
-        requestAnimationFrame(draw);
-    }
-    draw()
+    var timerId;
+    const fft$ = xs.create({
+        start: (listener) => {
+            function draw() {
+                analyser.getByteTimeDomainData(dataArray)
 
+                listener.next(dataArray)
+            }
+            timerId = setInterval(draw, 1000)
+        },
+
+        stop: () => {
+            clearInterval(timerId)
+        }
+    })
+
+    return fft$
 }
 
-function onSuccess(stream) {
-    startMicrophone(stream);
-}
-
-navigator.getUserMedia({audio: true}, onSuccess, console.error.bind(console));
+const userAudioMedia$ = xs.create({
+    start: listener => {
+        navigator.getUserMedia({audio: true},
+            stream => { listener.next(startMicrophone(stream)) } ,
+            console.error.bind(console)
+        )
+    },
+    stop: _ => {}
+})
 
 export function App (sources) {
-  const vtree$ = xs.of(
-    div('My Awesome Cycle.js app')
-  )
+
+  const fft$ = userAudioMedia$
+      .flatten()
+      .debug("fft")
+      .map(fft => fft.reduce((acc, value) => acc + value))
+      .map(sum => sum % usersImages.length)
+
+  const vtree$ = fft$
+  .map(index => div(usersImages[index]))
   const sinks = {
-    DOM: vtree$
+      DOM: vtree$
   }
   return sinks
 }
