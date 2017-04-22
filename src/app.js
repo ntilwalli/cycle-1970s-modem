f9f71f131d53615af4d38275979be5c5c5999e29
Thomas Belin <tom@doyoubuzz.com>
2017-04-22 10:32:55 +0200

    Test microphone

--- src/app.js
+++ src/app.js
@@ -1,6 +1,35 @@
 import {div} from '@cycle/dom'
 import xs from 'xstream'
 
+const audioCtx = new AudioContext();
+const analyser = audioCtx.createAnalyser();
+
+const BUFF_SIZE = 16384;
+
+function startMicrophone(stream){
+    source = audioCtx.createMediaStreamSource(stream);
+    source.connect(analyser);
+
+    analyser.fftSize = 2048;
+    var bufferLength = analyser.frequencyBinCount;
+    var dataArray = new Uint8Array(bufferLength);
+
+    function draw() {
+        analyser.getByteTimeDomainData(dataArray);
+
+        console.log(dataArray);
+        requestAnimationFrame(draw);
+    }
+    draw()
+
+}
+
+function onSuccess(stream) {
+    startMicrophone(stream);
+}
+
+navigator.getUserMedia({audio: true}, onSuccess, console.error.bind(console));
+
 export function App (sources) {
   const vtree$ = xs.of(
     div('My Awesome Cycle.js app')
