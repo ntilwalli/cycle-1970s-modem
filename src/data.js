37feebc0eda9963891eb2d0780e97f38cd94fee8
ArnaudBertrand <arnaudbertrand1@gmail.com>
2017-04-22 15:25:32 +0200

    adding user data

---
+++ src/data.js
@@ -0,0 +1,304 @@
+// Comand
+// copy(Array.prototype.slice.call($$('.people-modal-result')).map(x => ({
+//   src: x.querySelector('img').currentSrc,
+//   handle: x.querySelector('.people-modal-result__username').textContent,
+//   name: x.querySelector('h4').textContent,
+// })))
+
+export const users = [
+  {
+    "src": "https://avatars1.githubusercontent.com/u/974723?v=3&s=60",
+    "handle": "@scottcorgan",
+    "name": "Scott Corgan"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/398365?v=3&s=60",
+    "handle": "@Widdershin",
+    "name": "Nick Johnstone"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/11389?v=3&s=60",
+    "handle": "@brenthargrave",
+    "name": "Brent Hargrave"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/430934?v=3&s=60",
+    "handle": "@jonsharratt",
+    "name": "Jon Sharratt"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/959142?v=3&s=60",
+    "handle": "@cem2ran",
+    "name": "Cem Turan"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/1223735?v=3&s=60",
+    "handle": "@xlozinguez",
+    "name": "Xavier Lozinguez"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/47693?v=3&s=60",
+    "handle": "@sectore",
+    "name": "Jens Krause"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/389705?v=3&s=60",
+    "handle": "@nickbalestra",
+    "name": "Nick Balestra"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/90512?v=3&s=60",
+    "handle": "@staltz",
+    "name": "André Staltz"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/1620265?v=3&s=60",
+    "handle": "@vkarpov15",
+    "name": "Valeri Karpov"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/10159489?v=3&s=60",
+    "handle": "@drewdeal",
+    "name": "Drew Deal"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/1093738?v=3&s=60",
+    "handle": "@davidkpiano",
+    "name": "David Khourshid"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/736697?v=3&s=60",
+    "handle": "@whitecolor",
+    "name": "Alex"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/249641?v=3&s=60",
+    "handle": "@btipling",
+    "name": "Bjorn Tipling"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/635591?v=3&s=60",
+    "handle": "@mightyiam",
+    "name": "Shahar Or"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/11163901?v=3&s=60",
+    "handle": "@AdamWendell",
+    "name": "Adam Wendell"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/2760618?v=3&s=60",
+    "handle": "@michalvankodev",
+    "name": "Michal Vanko"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/701633?v=3&s=60",
+    "handle": "@FeliciousX",
+    "name": "Churchill Lee"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/1788218?v=3&s=60",
+    "handle": "@chadrien",
+    "name": "Hadrien de Cuzey"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/983050?v=3&s=60",
+    "handle": "@maxinteger",
+    "name": "László Vadász"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/8410254?v=3&s=60",
+    "handle": "@ntilwalli",
+    "name": "Nikhil Tilwalli"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/308196?v=3&s=60",
+    "handle": "@joaomilho",
+    "name": "Juan Lulkin"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/3234524?v=3&s=60",
+    "handle": "@fandaa",
+    "name": "František Polášek"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/1254146?v=3&s=60",
+    "handle": "@maximilianmikus",
+    "name": "Maximilian Götz-Mikus"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/618922?v=3&s=60",
+    "handle": "@SteveALee",
+    "name": "Steve Lee"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/2396926?v=3&s=60",
+    "handle": "@justinwoo",
+    "name": "Justin Woo"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/1015189?v=3&s=60",
+    "handle": "@learningjs",
+    "name": "learningjs"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/2698848?v=3&s=60",
+    "handle": "@marcusnielsen",
+    "name": "Marcus Nielsen"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/1456748?v=3&s=60",
+    "handle": "@mmrko",
+    "name": "Marko"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/4169710?v=3&s=60",
+    "handle": "@nikitadyumin",
+    "name": "nikitadyumin"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/142278?v=3&s=60",
+    "handle": "@aronallen",
+    "name": "Aron Nyborg Allen"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/8436403?v=3&s=60",
+    "handle": "@ohanhi",
+    "name": "Ossi Hanhinen"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/3530355?v=3&s=60",
+    "handle": "@chenxeed",
+    "name": "chenxeed"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/26493?v=3&s=60",
+    "handle": "@laszlokorte",
+    "name": "Laszlo Korte"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/535442?v=3&s=60",
+    "handle": "@lmatteis",
+    "name": "Luca Matteis"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/1142104?v=3&s=60",
+    "handle": "@danieltaborda",
+    "name": "Daniel Taborda"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/7959274?v=3&s=60",
+    "handle": "@raquelxmoss",
+    "name": "Raquel Moss"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/7339773?v=3&s=60",
+    "handle": "@saginadir",
+    "name": "Sagi"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/367036?v=3&s=60",
+    "handle": "@PalleZingmark",
+    "name": "Palle Zingmark"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/6529243?v=3&s=60",
+    "handle": "@bemayr",
+    "name": "Bernhard Mayr"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/3525105?v=3&s=60",
+    "handle": "@MariusKazlauskas",
+    "name": "Marius"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/4019680?v=3&s=60",
+    "handle": "@ArnaudBertrand",
+    "name": "Arnaud Bertrand"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/870945?v=3&s=60",
+    "handle": "@pascalgermain",
+    "name": "Pascal Germain"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/1203949?v=3&s=60",
+    "handle": "@jevakallio",
+    "name": "Jani Eväkallio"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/2212006?v=3&s=60",
+    "handle": "@bahmutov",
+    "name": "Gleb Bahmutov"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/7112838?v=3&s=60",
+    "handle": "@steffipeters",
+    "name": "Stephanie Nyborg Allen"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/917061?v=3&s=60",
+    "handle": "@Ozzee",
+    "name": "Oskar Ehnström"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/9034374?v=3&s=60",
+    "handle": "@markjose",
+    "name": "Mark Jose"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/1090716?v=3&s=60",
+    "handle": "@atomrc",
+    "name": "Thomas Belin"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/574438?v=3&s=60",
+    "handle": "@O-Zone",
+    "name": "Hasse Feldthaus"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/4251?v=3&s=60",
+    "handle": "@Vinnl",
+    "name": "Vincent"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/5254755?v=3&s=60",
+    "handle": "@steffenagger",
+    "name": "Steffen Agger"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/17760395?v=3&s=60",
+    "handle": "@kiprijonas",
+    "name": "kiprijonas"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/5277979?v=3&s=60",
+    "handle": "@SebastianFlo",
+    "name": "Sebastian Florian"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/3091087?v=3&s=60",
+    "handle": "@artfuldev",
+    "name": "Sudarsan Balaji"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/4084212?v=3&s=60",
+    "handle": "@fanderzon",
+    "name": "Fredrik Andersson"
+  },
+  {
+    "src": "https://avatars1.githubusercontent.com/u/1529052?v=3&s=60",
+    "handle": "@jvanbruegge",
+    "name": "Jan van Brügge"
+  },
+  {
+    "src": "https://avatars2.githubusercontent.com/u/8846179?v=3&s=60",
+    "handle": "@Petroochio",
+    "name": "Peter Gyory"
+  },
+  {
+    "src": "https://avatars0.githubusercontent.com/u/16745118?v=3&s=60",
+    "handle": "@festinalent3",
+    "name": "Emma Sjöström"
+  }
+]
